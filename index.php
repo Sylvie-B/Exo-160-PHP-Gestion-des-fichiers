39900564c0eceec30831386731feeb3c72931478
Sylvie-B <unsolo@hotmail.fr>
2021-01-29 13:37:22 +0100

    écrire dans un fichier (le créer), ajouter des mots, affiche extension, vérif si fichier existe

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
  * --> Attention à bien fermer votre fichier si vous utilisez fopen()
  */
 // TODO Votre code ici.
-
+$content = "Omnes hora vulnerant ultima necat.";
+file_put_contents('monText.txt', $content);
 
 /**
  * 3. Créez un tableau contenant au moins 10 chaînes de caractère au choix.
@@ -15,20 +16,29 @@
  * --> Attention: les chaînes de caractères doivent s'ajouter à la suite du contenu déjà existant, pas d'écrasement.
  */
 // TODO Votre code ici
+$tab = ["lorem", "ipsum", "dolor", "sit", "amet", "elit", "ducimus", "fugit", "illum", "ipsam"];
+$file = fopen('monText.txt', 'a+b');
 
+foreach ($tab as $word){
+    fwrite($file, $word);
+}
 
+fclose($file);
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
-
-
+$way = pathinfo('index.php');
+echo $way['extension'];
+echo "<br><br>";
 
 /**
  * 6. Testez si le fichier 'toto' existe, sil n'existe pas, afficher un texte distant que ce fichier n'existe pas !
  */
 // TODO Votre code ici.
-
+if(!file_exists('toto.txt')){
+    echo "Le fichier n'existe pas !";
+}
 
 /**
  * Super bonus.
